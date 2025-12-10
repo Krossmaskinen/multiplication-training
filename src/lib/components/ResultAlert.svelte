4ed08a63a54b3f0448cf8cf0b0650e721c89f404
bill kurtson <bill.kurtson@gmail.com>
2025-12-10 14:33:07 +0100

    feat: Show cute cat image on right answer

--- src/lib/components/ResultAlert.svelte
+++ src/lib/components/ResultAlert.svelte
@@ -7,23 +7,26 @@
 	}
 
 	let { result, showResultBanner = false }: Props = $props();
+
+	const cuteCatImages = [
+		'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?w=100&h=100&fit=crop',
+		'https://images.unsplash.com/photo-1574158622682-e40e69881006?w=100&h=100&fit=crop',
+		'https://images.unsplash.com/photo-1596854407944-bf87f6fdd49e?w=100&h=100&fit=crop',
+		'https://images.unsplash.com/photo-1513245543132-31f507417b26?w=100&h=100&fit=crop',
+		'https://images.unsplash.com/photo-1571566882372-1598d88abd90?w=100&h=100&fit=crop',
+		'https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?w=100&h=100&fit=crop'
+	];
+
+	const randomCatImage = cuteCatImages[Math.floor(Math.random() * cuteCatImages.length)];
 </script>
 
 {#if result.isCorrect === true && showResultBanner}
 	<div class="alert alert-success">
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			class="stroke-current shrink-0 h-6 w-6"
-			fill="none"
-			viewBox="0 0 24 24"
-		>
-			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-			/>
-		</svg>
+		<img
+			src={randomCatImage}
+			alt="Cute cat"
+			class="w-12 h-12 rounded-full object-cover shrink-0"
+		/>
 		<span>Rätt!</span>
 	</div>
 {:else if result.isCorrect === false && showResultBanner}
